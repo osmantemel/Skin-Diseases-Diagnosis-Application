baa3e7c4d43e3f40e9f00cc10467378961bd96a4
osmantemel <temelosman778@gmail.com>
2024-04-27 15:52:12 +0300

    changes saved .

--- backend/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ backend/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300d7addd3c7952fd3eb5e1229deb647cdc78171")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
